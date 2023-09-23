4e831ea310b569ad83e25c88cefdd0868ec30ce3
kujel-selsuru <79940813+kujel-selsuru@users.noreply.github.com>
2023-09-23 03:20:13 -0700

    Add files via upload

---
+++ LayeredDrawing.cs
@@ -0,0 +1,123 @@
+﻿//====================================================
+//Written by Kujel Selsuru
+//Last Updated 23/09/23
+//====================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XenoLib
+{
+    public class DrawingNode
+    {
+        public XenoSprite sprite;
+        public int priority;
+        public DrawingNode next;
+        /// <summary>
+        /// DrawingNode constructor
+        /// </summary>
+        /// <param name="sprite">XenoSprite reference</param>
+        /// <param name="priority">Rendering priority value</param>
+        /// <param name="next">Next DrawingNode</param>
+        /// <param name="prev">Prev DrawingNode</param>
+        public DrawingNode(XenoSprite sprite, int priority, DrawingNode next = null)
+        {
+            this.sprite = sprite;
+            this.priority = priority;
+            this.next = next;
+        }
+    }
+
+    public static class LayeredDrawing
+    {
+        //private
+        private static DrawingNode start;
+        private static int index;
+
+        //public
+        /// <summary>
+        /// Initializes LayeredDrawing
+        /// </summary>
+        public static void init()
+        {
+            start = null;
+            index = 0;
+        }
+        /// <summary>
+        /// Add a sprite to rendering list
+        /// </summary>
+        /// <param name="sprite">XenoSprite reference</param>
+        /// <param name="priority">Rendering priority value</param>
+        public static void addSprite(XenoSprite sprite, int priority)
+        {
+            DrawingNode node = new DrawingNode(sprite, priority);
+            DrawingNode current = start;
+            if(start != null)
+            {
+                if(current != null)
+                {
+                    while(current != null && current.priority <= priority)
+                    {
+                        current = current.next;
+                    }
+                    if(current != null)
+                    {
+                        node.next = current.next;
+                        current.next = node;
+                        index++;
+                    }
+                }
+            }
+            else
+            {
+                start = node;
+            }
+        }
+        /// <summary>
+        /// Draws sprites in rendering list
+        /// </summary>
+        /// <param name="renderer">Renderer reference</param>
+        /// <param name="winx">Window X offset</param>
+        /// <param name="winy">Window Y offset</param>
+        public static void drawSprites(IntPtr renderer, int winx, int winy)
+        {
+            while(start != null)
+            {
+                if(start.sprite != null)
+                {
+                    start.sprite.draw(renderer, winx, winy);
+                    start = start.next;
+                    index--;
+                }
+                else
+                {
+                    index--;
+                    break;
+                }
+            }
+        }
+        /// <summary>
+        /// Clears drawing jobs
+        /// </summary>
+        public static void clearJobs()
+        {
+            if(start != null)
+            {
+                while(start.next != null)
+                {
+                    start = start.next;
+                    index--;
+                }
+            }
+        }
+        /// <summary>
+        /// JobCount property
+        /// </summary>
+        public static int JobCount
+        {
+            get { return index; }
+        }
+    }
+}
---
+++ LayeredSpriteRenderer.cs
@@ -0,0 +1,54 @@
+﻿//====================================================
+//Written by Kujel Selsuru
+//Last Updated 23/09/23
+//====================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDL2;
+
+namespace XenoLib
+{
+    public static class LayeredSpriteRenderer
+    {
+        public static List<XenoSprite> sprites;
+        /// <summary>
+        /// Draws sprites in a layered format to provide a depth effect
+        /// </summary>
+        /// <param name="renderer">Renderer reference</param>
+        /// <param name="wx">Window X value in pixels</param>
+        /// <param name="wy">Window Y value in pixels</param>
+        /// <param name="ww">Window width value in pixels</param>
+        /// <param name="wh">Window height value in pixels</param>
+        /// <param name="scaler">Tile height/scaling value</param>
+        public static void renderSprites(IntPtr renderer, 
+            int wx, int wy, int ww, int wh, int scaler = 32)
+        {
+            if(sprites != null)
+            {
+                //range of layers to render
+                for(int i = wy / scaler; i < (wy / scaler) + (wh / scaler); i++)
+                {
+                    //list of sprites to render
+                    for(int k = 0; k < sprites.Count; k++)
+                    {
+                        //sprite is on screen
+                        if(sprites[k].X >= wx && sprites[k].X < wx + ww)
+                        {
+                            if(sprites[k].Y >= wy && sprites[k].Y < wy + wh)
+                            {
+                                //sprite's bottom edge
+                                if(sprites[k].Bottom / 32 == i)
+                                {
+                                    sprites[k].draw(renderer, wx, wy);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
